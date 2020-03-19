1545c52a1c2c5a0418fe40a838b7147622191790
innerpeacez <innerpeace.zhai@gmail.com>
2020-03-19 14:20:01 +0800

    fix

--- test/scripts/skywalking-deploy.sh
+++ test/scripts/skywalking-deploy.sh
@@ -76,14 +76,15 @@ echo "Skywalking ES7 Deploy"
 helm -n $SKYWALKING_ES7_NAMESPACE install skywalking skywalking --set oap.replicas=1 --set elasticsearch.replicas=1
 
 for component in $NEED_CHECK_PREFIX"oap" ; do
-  sleep 60
+  sleep 600
 #  for (( i = 0; i < 5; i++ )); do
 #      kubectl get pod -o wide -n ${SKYWALKING_ES7_NAMESPACE}
 #      sleep 10
 #      kubectl logs elasticsearch-master-0 -n ${SKYWALKING_ES7_NAMESPACE}
 #  done
   kubectl describe pod/elasticsearch-master-0 -n ${SKYWALKING_ES7_NAMESPACE}
-  kubectl logs elasticsearch-master-0 -n ${SKYWALKING_ES7_NAMESPACE} -f
+#  kubectl logs elasticsearch-master-0 -n ${SKYWALKING_ES7_NAMESPACE} -f
+  kubectl get all -n ${SKYWALKING_ES7_NAMESPACE}
   kubectl -n ${SKYWALKING_ES7_NAMESPACE} wait $component --for condition=available --timeout=600s
   kubectl -n ${SKYWALKING_ES6_NAMESPACE} wait $component --for condition=available --timeout=600s
 done
